9bc025f9070788e80fe0ade4de8aea645ab0b0a5
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-09-12 22:49:49 +0300

    feat: init htmx learning

---
+++ cmd/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+type Count struct {
+	Count int
+}
+
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func newContact(name string, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("A", "a@a.com"),
+			newContact("B", "b@b.com"),
+			newContact("C", "c@c.com"),
+		},
+	}
+}
+
+func main() {
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	data := newData()
+
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", data)
+	})
+
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		data.Contacts = append(data.Contacts, newContact(name, email))
+
+		return c.Render(http.StatusOK, "index", data)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
